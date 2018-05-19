2698f85ac696c0da1e181e0e366d63e980a58ce1
buchi90 <buchibabu225@gmail.com>
2018-05-19 23:56:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
+
 #!/bin/sh
-sudo ln -s /var/lib/jenkins/workspace/helloworld-tomcat/target ~/deploy
-cd  /root/deploy
-sudo mv spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT.war helloworld.war
-sudo chmod 666 helloworld.war
-sudo scp -r helloworld.war 172.18.1.2:/usr/share/tomcat/webapps/
-sudo ssh root@172.18.1.2 "service tomcat stop"
-sudo ssh root@172.18.1.2 "service tomcat start"
-sudo rm -rf /root/deploy
-
-
+cd /var/lib/jenkins/workspace/appdepl/target
+mv spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT.war helloworld.war
+scp -v -o StrictHostKeyChecking=no helloworld.war root@192.168.56.2:/usr/share/tomcat/webapps
+ssh root@192.168.56.2 "service tomcat stop"
+ssh root@192.168.56.2 "service tomcat start"
