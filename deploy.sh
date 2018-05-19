9937ff52f4365c75bba4a03cc928562c2e1a5442
buchi90 <buchibabu225@gmail.com>
2018-05-20 00:35:42 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /var/lib/jenkins/workspace/appdepl/target
+mv spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT.war helloworld.war
+scp -v -o StrictHostKeyChecking=no helloworld.war root@192.168.56.2:/usr/share/tomcat/webapps
+ssh root@192.168.56.2 "service tomcat stop"
+ssh root@192.168.56.2 "service tomcat start"
+
+
